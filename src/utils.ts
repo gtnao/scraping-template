3f662da327db072161c2018bcbd669ad3d7c98ed
naotaka nakane <n.nakane0219@gmail.com>
2024-08-17 13:54:24 +0000

    Add date-fns.

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
+import { format, eachDayOfInterval } from "date-fns";
+
 export async function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function getFormattedDates(start: Date, end: Date): string[] {
+  return eachDayOfInterval({ start, end }).map((date) =>
+    format(date, "yyyy-MM-dd"),
+  );
 }
